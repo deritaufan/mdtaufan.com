6add3906cc33a35cfa0679922b4ec7df0c276af7
M Deri Taufan <deritaufan@gmail.com>
2016-02-06 14:56:42 +0000

    Fixed javascript on successful contact message

--- app/assets/javascripts/contact_me.js
+++ app/assets/javascripts/contact_me.js
@@ -6,6 +6,9 @@ $(document).ready(function() {
             $($form).on("ajax:success",function(e, data, status, xhr){
                 try{
                     var respone = JSON.parse(xhr.responseText);
+                    if ($("#result").length > 0) {
+                        $("div").remove(".alert, .alert-success");
+                    }
                     $("#result").append(respone.html);
                     $($form).trigger("reset");
                 } catch(e) {
